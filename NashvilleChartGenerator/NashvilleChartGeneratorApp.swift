787b12289121b7df91f1a61241f00405b8c5d28e
Michael Lee <michael@michaelleemusic.com>
2025-07-26 22:50:16 -0700

    Initial Commit

---
+++ NashvilleChartGenerator/NashvilleChartGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NashvilleChartGeneratorApp.swift
+//  NashvilleChartGenerator
+//
+//  Created by Michael Lee on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NashvilleChartGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
